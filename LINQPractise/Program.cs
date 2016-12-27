9a1b497774ee2e8e0e383d74578d8f8484d2d120
rasel01 <raseluiu.ac.bd@gmail.com>
2016-12-27 09:22:05 +0600

    Practise 1

---
+++ LINQPractise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new Student();
+            // old process using Foreach loop :
+
+            //foreach (Student stud in std.GetStudentList())
+            //{
+            //    Console.WriteLine("Student ID :"+stud.Id+ Environment.NewLine 
+            //        +"Student Name:" + stud.Name + Environment.NewLine + "Student Age :" + stud.Age + Environment.NewLine);
+            //}
+
+
+
+            // LINQ Process :
+              
+            // SQL LIKE SYNTAX:
+
+           var stdList = from variable in std.GetStudentList()
+                         select  variable;
+            Console.WriteLine(stdList);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LINQPractise/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQPractise
+{
+    class Student
+    {
+        private readonly List<Student> _students;
+        public Student()
+        {
+            _students = new List<Student>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public List<Student> GetStudentList()
+        {
+            _students.Add(new Student() { Id = 1, Name = "rasel", Age = 25 });
+            _students.Add(new Student() { Id = 2, Name = "arnold", Age = 37 });
+            _students.Add(new Student() { Id = 3, Name = "crow", Age = 50 });
+            _students.Add(new Student() { Id = 4, Name = "Andrew", Age = 32 });
+            _students.Add(new Student() { Id = 5, Name = "Expoler", Age = 16 });
+
+            return _students;
+        }
+
+        public void AddStudent(Student stdObject)
+        {
+            _students.Add(stdObject);
+        }
+
+
+
+    }
+}
